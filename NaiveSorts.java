3048be06870314ef727791f96703ef5e899e2e97
Sophie Peneva <39370712+sophiepeneva@users.noreply.github.com>
2019-07-17 21:05:47 +0300

    Create NaiveSorts.java

---
+++ NaiveSorts.java
@@ -0,0 +1,42 @@
+# Sorts
+
+public class NaiveSorts {
+
+private static void swap(int a, int b, int[] arr) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j + 1] < arr[j]) {
+                    swap(j + 1, j, arr);
+                }
+            }
+        }
+    }
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0 && arr[j] < arr[j - 1]; j--) {
+                swap(j, j - 1, arr);
+            }
+        }
+
+    }
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int index = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[index]) {
+                    index = j;
+                }
+            }
+            swap(i, index, arr);
+        }
+    }
+
+}
